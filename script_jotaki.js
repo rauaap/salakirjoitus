515ca5e2c4c5e07fd02bed7e5c7f58a53366a75e
Ekicksi <100118771+Ekicksi@users.noreply.github.com>
2022-03-29 11:27:48 +0300

    Update script_jotaki.js

--- script_jotaki.js
+++ script_jotaki.js
@@ -35,3 +35,17 @@ function SwapKeyValuePairs() {
 //         return this.crypted ? this.input : crypt(reverse = false);
 //     }
 // }
+
+function upperCaseFirstLetter(input) {
+    input = input.toLowerCase();
+    let sanat = input.split(" ");
+
+    for (i = 0; i < sanat.length; i++)
+    {
+        sanat[i] = sanat[i].replace(sanat[i].charAt(0), sanat[i].charAt(0).toUpperCase()); 
+        //Käy läpi taulukon jokaisen sanan ensimmäisen kirjaimen ja korvaa sen vastaavalla isolla kirjaimella. 
+    }
+
+    let output = sanat.join(" ");
+    return output;
+}
